dc4ef37cb74beef1f55b571b2d049b23ab852ad1
Aleksei Erofeev <eao173a@gmail.com>
2024-09-22 22:35:36 +0800

    Использует метод массива map в функции Tenets(), чтобы рендерить код при загрузке.

--- src/components/Tenets.tsx
+++ src/components/Tenets.tsx
@@ -1,33 +1,30 @@
 export default function Tenets() {
+  const array = [
+    "Давно хотели попробовать йогу, но не решались начать",
+    "Хотите укрепить позвоночник, избавиться от болей в спине и суставах",
+    "Ищете активность, полезную для тела и души",
+  ];
+
+  const styleBlock =
+    "bg-[linear-gradient(115.81deg,_#151720_34.98%,_#1E212E_91.5%)] rounded-[28px] min-w-[320px] min-h-[141px] p-5 flex-grow flex-shrink-0 max-w-[369px]";
+
   return (
     <section>
-      <h2 className="text-[40px] font-semibold leading-[44px]">Подойдет для вас, если:</h2>
+      <h2 className="text-[40px] font-semibold leading-[44px]">
+        Подойдет для вас, если:
+      </h2>
       <div className="flex flex-wrap gap-[17px] text-white flex-row pt-10">
-        <div
-          className="bg-[linear-gradient(115.81deg,_#151720_34.98%,_#1E212E_91.5%)] rounded-[28px] w-[369px] h-[141px] p-5">
-          <div className="flex gap-[25px] items-center h-[101px]">
-            <p className="text-[#BCEC30] text-[75px] leading-[75px]">1</p>
-            <p className="text-[24px] font-normal break-words">Давно хотели попробовать йогу,<br /> но не решались
-              начать</p>
+        {array.map((text, index) => (
+          <div key={index} className={styleBlock}>
+            <div className="flex gap-[25px] items-center h-full">
+              <p className="text-[#BCEC30] text-[75px] leading-[75px]">
+                {index + 1}
+              </p>
+              <p className="text-[24px] font-normal break-words">{text}</p>
+            </div>
           </div>
-        </div>
-        <div
-          className="bg-[linear-gradient(115.81deg,_#151720_34.98%,_#1E212E_91.5%)] rounded-[28px] w-min-[320px] h-[141px] p-5">
-          <div className="flex gap-[25px] items-center h-[101px]">
-            <p className="text-[#BCEC30] text-[75px] leading-[75px]">2</p>
-            <p className="text-[24px] font-normal break-words">Хотите укрепить<br /> позвоночник, избавиться<br /> от
-              болей в спине и суставах</p>
-          </div>
-        </div>
-        <div
-          className="bg-[linear-gradient(115.81deg,_#151720_34.98%,_#1E212E_91.5%)] rounded-[28px] w-min-[320px] h-[141px] p-5">
-          <div className="flex gap-[25px] items-center h-[101px]">
-            <p className="text-[#BCEC30] text-[75px] leading-[75px]">3</p>
-            <p className="text-[24px] font-normal break-words">Ищете активность,<br /> полезную для тела<br /> и души
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
-  )
+  );
 }
