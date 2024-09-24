c81a9b1894298565162ed9770ceef46c9b580086
Acer <regina-sabitova-1998@mail.ru>
2024-09-24 13:30:55 +0500

    готовая версия профиля

--- src/components/ProfileInfo.tsx
+++ src/components/ProfileInfo.tsx
@@ -1,23 +1,31 @@
 import Image from "next/image";
+import ButtonLink from "./ui/ButtonLink";
 
 export default function ProfileInfo() {
   return (
-    <div className="bg-white rounded-3xl h-[257px]">
-      <div className="flex flex-row  p-[30px]  w-[624px] h-[197px]">
-        <Image
-          src="/img/profile.svg"
-          alt="Фото профиля"
-          width={197}
-          height={197}
-        />
-        <div className="flex flex-col h-48 w-96">
-          <h1 className="text-[32px]">Сергей</h1>
-
-          <p className="text-[18px]">Логин: sergey.petrov96</p>
-          <p className="text-[18px]">Пароль: 4fkhdj880d</p>
-
-          <button className="px-[28px] w-[192px] h-[52px] py-4 rounded-[46px] bg-[#BCEC30] hover:bg-[#C6FF00] transition-colors duration-300 ease-linear">Изменить пароль</button>
-          <button className="px-[28px] w-[192px] h-[52px] py-4 rounded-[46px] transition-colors duration-300 ease-linear">Выйти</button>
+    <div className="bg-[#FFFFFF] rounded-[30px] sm:px-[30px] px-[10px] py-[30px]">
+      <div className="flex flex-wrap flex-row sm:space-x-[33px]">
+        <div className="relative sm:w-[197px] w-[141px] sm:h-[197px] h-[141px] sm:mx-[0px] mx-[90px]">
+          <Image
+            src="/img/profile.png"
+            alt="Фото профиля"
+            /* className="w-full h-full object-cover" */
+            width={197}
+            height={197}
+          />
+        </div>
+        <div className="flex flex-col sm:gap-[20px] gap-[13px] sm:mt-0 mt-[22px] sm:ml-0 ml-[19px]">
+          <div className="sm:text-[32px] text-[24px] font-bold">Сергей</div>
+          <div className="flex flex-col gap-[2px]">
+            <p className="text-[18px]">Логин: sergey.petrov96</p>
+            <p className="text-[18px]">Пароль: ***********</p>
+          </div>
+          <div className="flex flex-wrap flex-col align-center md:flex-row gap-[15px]">
+            <ButtonLink text={"Изменить пароль"} />
+            <button className="w-[192px] h-[52px] mt-6 px-[28px] py-4 rounded-[46px] border border-black  text-[#000000] hover:bg-[#F7F7F7] active:bg-[#E9ECED] cursor-custom">
+              Выйти
+            </button>
+          </div>
         </div>
       </div>
     </div>
