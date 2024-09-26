9ea6c02d5e2d4e4151d725a714c3f85d75f002d7
OlegGorin <olgorin@rambler.ru>
2024-09-26 22:50:05 +0200

    Верул стартовую страницу после тестирования

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-// import Main from "@/components/Main";
-import ProfileInfo from "@/components/Profile";
+import Main from "@/components/Main";
+// import ProfileInfo from "@/components/Profile";
 
 export default function Home() {
-  // return <Main />;
-  return <ProfileInfo />;
+  return <Main />;
+  // return <ProfileInfo />;
 }
